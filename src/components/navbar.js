a7c90346b5df89bc5d5f11574a3ccbf9d38e0851
Anonymous <Anonymous@DESKTOP-GLJRC7M>
2023-07-25 18:48:53 +0100

    Adding Navigation Bar (not-active)

---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import favicon from './favicon.png';
+function Navbar() {
+    return (
+    <div className="Navbar">
+      <header className="App-header">
+      <nav className="navbar navbar-dark navbar-expand bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/#"><img src={favicon} width="40" height="40" alt="27"></img></a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                  <li className="nav-item"><a className="nav-link active" href="/#">Home</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/#">Page 1</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/#">Page 2</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/#">Page 3</a></li>
+              </ul>
+          </div>
+        </div>
+      </nav>
+      </header>
+    </div>
+    );
+}
+
+export default Navbar;
