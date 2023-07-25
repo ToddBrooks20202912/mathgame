51e077a6fc9918c58eea09e7c59c126d46fcd7c8
Anonymous <Anonymous@DESKTOP-GLJRC7M>
2023-07-25 18:49:35 +0100

    Displaying generator on main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import QuestionGenerator from './components/questionGenerator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Example.
         </p>
+        <QuestionGenerator />
       </header>
     </div>
   );
