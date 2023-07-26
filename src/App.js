6d57365b204d6226c28c02048935fc303b2492b6
Anonymous <Anonymous@DESKTOP-GLJRC7M>
2023-07-26 17:31:34 +0100

    Todo-instructions on App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Navbar from './components/navbar';
 import QuestionGenerator from './components/questionGenerator';
+import Grid from './components/grid';
 
 
 function App() {
+
+  /* Display the grid. The question is generated via the QuestionGenerator file, then the question can be stored within the state of the main App.
+  After this, the grid's individual rows are then checked when complete. This check combines each value of the columns of the rows (1, 2, 3, etc. )
+  then runs eval() on this string (like the QuestionGenerator does). You could pass the answer to the App's state when passing the question, but this
+  might not be necessary. The question is the only thing which needs to match. The answer could be determined within the App.  */
+
   return (
     <div className="App">
       <div className="Navbar">
@@ -13,12 +20,11 @@ function App() {
       </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Example.
-        </p>
         <QuestionGenerator />
       </header>
+      <body className="App-body">
+        <Grid />
+      </body>
     </div>
   );
 }
