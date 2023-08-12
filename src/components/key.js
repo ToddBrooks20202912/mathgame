2b0f91a63171ffc17f61e421653be53b6bcb2c82
Anonymous <Anonymous@DESKTOP-GLJRC7M>
2023-08-12 10:52:14 +0100

    Key logic for touch controls/click

--- src/components/key.js
+++ src/components/key.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
-function Key({keyMap}) {
-    return <div className="numKey">{keyMap}</div>
+function Key({keyMap, twoKey}) {
+    const {grid, setGrid, attemptPos, setPos} = useContext(AppContext);
+    const selectNum = () => {
+        if (keyMap == "ENT") {
+            if (attemptPos.keyPos != 5) return;
+            setPos({rowPos: attemptPos.rowPos + 1, keyPos: 0}) // move to the next row
+        } else if (keyMap == "DEL") {
+            if (attemptPos.keyPos == 0) return;
+            const statusGrid = [...grid]; // current board at this moment
+            statusGrid[attemptPos.rowPos][attemptPos.keyPos] = ""; // deleting/clearing the key stored at this position
+            setGrid(statusGrid); // push new grid 
+            setPos({...attemptPos, keyPos: attemptPos.keyPos - 1}); // pushes
+        } else {
+            if (attemptPos.keyPos > 4) { // end of the row
+                return;
+            }   
+            else {
+                const statusGrid = [...grid]; // current board at this moment
+                statusGrid[attemptPos.rowPos][attemptPos.keyPos] = keyMap; // clicked key = key on row
+                setGrid(statusGrid); // push new grid 
+                setPos({...attemptPos, keyPos: attemptPos.keyPos + 1}); // currently runs through the entire row, does not actually transfer to the next "attempt" (which is row num)
+            }
+        }
+        
+       
+    }
+    return <div className="numKey" id={twoKey && "twoKey"} onClick={selectNum}>
+                {keyMap}
+            </div>
 }
 
 export default Key;
--- src/components/numpad.js
+++ src/components/numpad.js
@@ -7,12 +7,12 @@ function Numpad() {
     const layout2 = ["7", "8", "9"];
     const layout3 = ["4", "5", "6"];
     const layout4 = ["1", "2", "3"];
-    const layout5 = ["0", "+", "ENT"];
-    const layout6 = ["DEL"];
+    const layout5 = ["0", "+"];
+    const layout6 = ["DEL", "ENT"];
 
     return (
         <div className='Numpad'>
-            <div className='padRow'> {layout1.map((key) => {
+            <div className='padRow'> {layout1.map((key) => {  { /* iterate through array and output mapped keys */ }
                 return <Key keyMap={key}/>;
             })}
             </div> {/* / * - */}
@@ -20,21 +20,13 @@ function Numpad() {
                 return <Key keyMap={key}/>;
             })}
             </div> {/* 7 8 9 */}
-            <div className='padRow'> {layout3.map((key) => {
-                return <Key keyMap={key}/>;
-            })}
+            <div className='padRow'> {layout3.map((key) => {return <Key keyMap={key}/>;})}
             </div> {/* 4 5 6 */}
-            <div className='padRow'> {layout4.map((key) => {
-                return <Key keyMap={key}/>;
-            })}
+            <div className='padRow'> {layout4.map((key) => {return <Key keyMap={key}/>;})}
             </div> {/* 1 2 3 */}
-            <div className='padRow'> {layout5.map((key) => {
-                return <Key keyMap={key}/>;
-            })}
+            <div className='padRow'> {layout5.map((key) => {return <Key keyMap={key} twoKey={true}/>;})}
             </div> {/* 0 + Enter */}
-            <div className='padRow'> {layout6.map((key) => {
-                return <Key keyMap={key}/>;
-            })}
+            <div className='padRow'> {layout6.map((key) => {return <Key keyMap={key} twoKey={true}/>;})}
             </div> {/* DEL (Backspace) */}
         </div>
     )
