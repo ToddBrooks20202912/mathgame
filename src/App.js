4e2e9c8556922ebc6768703db0d38b786cb07784
Anonymous <Anonymous@DESKTOP-GLJRC7M>
2023-08-12 10:50:14 +0100

    Cleared up clutter

--- src/App.js
+++ src/App.js
@@ -14,32 +14,29 @@ export const AppContext = createContext();
 
 function App() {
   const [grid, setGrid] = useState(gridReset);
-
+  const [attemptPos, setPos] = useState({rowPos:0 , keyPos: 0});
   /* then the question can be stored within the state of the main App.
    are then checked when complete. This check combines each value of the columns of the rows (1, 2, 3, etc. )
   then runs eval() on this string (like the QuestionGenerator does). You could pass the answer to the App's state when passing the question, but this
   might not be necessary. The question is the only thing which needs to match. The answer could be determined within the App.  */
   return (
     <div className="App">
-      <div className="Navbar">
       <Navbar />
-      </div>
-      <div className='App-Logo-Container'>
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
+      <AppContext.Provider value={{grid, setGrid, attemptPos, setPos}}> {/* Push state using Context, allows components down the line to access state functions */}
       <header className="App-header">
-        
-        <AppContext.Provider value={{grid, setGrid}}> {/* Push state using Context, allows components down the line to access state functions */}
-          <QuestionGenerator /> {/* The question is generated via the QuestionGenerator file */}
-          <Grid /> {/* Display the grid. */}
-        </AppContext.Provider> 
+      <QuestionGenerator /> {/* The question is generated via the QuestionGenerator file */}
+        <Grid /> {/* Display the grid. */}
         <Numpad />
         <Sounds />
-        
       </header>
+      </AppContext.Provider> 
     </div>
   );
 }
+
+// <div className='App-Logo-Container'>
+//<img src={logo} className="App-logo" alt="logo" />
+//</div>
 
 // if guessed word = correct
   // else increase attempt
